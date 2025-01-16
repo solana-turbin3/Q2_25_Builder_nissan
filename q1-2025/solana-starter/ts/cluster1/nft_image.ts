d24bf9e4bd18e966397825b047941dbaf426c37d
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-01-16 11:15:26 -0400

    do nft image

--- q1-2025/solana-starter/ts/cluster1/nft_image.ts
+++ q1-2025/solana-starter/ts/cluster1/nft_image.ts
@@ -3,6 +3,8 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
+import * as path from 'path'
+
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -16,13 +18,14 @@ umi.use(signerIdentity(signer));
 (async () => {
     try {
         //1. Load image
+        const imagePath = path.resolve(__dirname, 'generug.png');
+        console.log("Attempting to read image from:", imagePath);
         //2. Convert image to generic file.
+        const image = await readFile(imagePath);
+        const myUri = createGenericFile(image, "MyRug", {contentType: "image/png"})
         //3. Upload image
-
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        const uri = await umi.uploader.upload([myUri])
+        console.log("Your image URI: ", uri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
