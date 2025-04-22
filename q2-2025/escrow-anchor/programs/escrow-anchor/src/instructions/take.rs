6c9328b1588b72b254aca9c3b128afceea667392
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-04-22 20:24:06 +1000

    do escrow hw

---
+++ q2-2025/escrow-anchor/programs/escrow-anchor/src/instructions/take.rs
@@ -0,0 +1,55 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked, close_account, CloseAccount},
+};
+use crate::state::Escrow;
+
+
+#[derive(Accounts)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+
+    #[account(
+        mint::token_program = token_program,
+    )]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mint::token_program = token_program,
+    )]
+    pub mint_b: InterfaceAccount<'info, Mint>,
+
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program,
+    )]
+    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+        
+}
